e08d3caafe0ef95981947fb2c68601fa23fa5476
miyang2023 <miyangfabiola@gmail.com>
2024-12-26 23:46:38 -0500

    Update variables.tf

--- modules/jenkins-server/variables.tf
+++ modules/jenkins-server/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "The AMI ID for the Jenkins server"
   type        = string
-  default     = "ami-0f226ae5ce4b11922"
+  default     = "ami-0e2c8caa4b6378d8c"
 }
 
 variable "instance_type" {
@@ -13,7 +13,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "The key name for the Jenkins server"
   type        = string
-  default     = "DevOps_Train2024"
+  default     = "faby_Train2024"
 }
 
 variable "main-region" {
