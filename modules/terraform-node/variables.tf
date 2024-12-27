f5d4f2c06f84e4d8b8255dcbe7e8440a669693d5
miyang2023 <miyangfabiola@gmail.com>
2024-12-27 12:07:55 -0500

    Update variables.tf

--- modules/terraform-node/variables.tf
+++ modules/terraform-node/variables.tf
@@ -1,7 +1,7 @@
 variable "ami_id" {
   description = "The AMI ID for the Terraform node"
   type        = string
-  default     = "ami-03c983f9003cb9cd1"
+  default     = "ami-0e2c8caa4b6378d8c"
 }
 
 variable "instance_type" {
@@ -13,7 +13,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "The key name for the Jenkins server"
   type        = string
-  default     = "DevOps_Train2024"
+  default     = "faby_Shopping2025"
 }
 
 variable "main-region" {
